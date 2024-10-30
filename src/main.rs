4e62d0ffcb86641c4ba8b2cee0cf7b267c99b8d6
Brenzee <karelisbrendons@gmail.com>
2024-10-30 09:20:55 +0200

    univ3 test example

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,14 @@ use revm::{
     primitives::{AccountInfo, Bytes, ExecutionResult, Output},
     Evm,
 };
-use revm_actions::univ2_swap;
+use revm_actions::{balance_of, get_univ2_reserves, univ2_swap};
 use uniswapv2::{get_weth_pair, UniV2Pair};
+use uniswapv3::test_swap;
 
 mod erc20;
 mod revm_actions;
 mod uniswapv2;
+mod uniswapv3;
 
 // TODO:
 // - Take RPC_URL from env variables. Currently using local Reth node.
@@ -32,6 +34,7 @@ mod uniswapv2;
 
 const WETH: Address = address!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
 const DEFAULT_ACC: Address = address!("e4A6aD6E1B86AB8f2d2f571717592De46bFaF614");
+
 type AlloyCacheDB = CacheDB<AlloyDB<Http<Client>, Ethereum, RootProvider<Http<Client>>>>;
 
 #[derive(Parser, Debug)]
@@ -87,26 +90,30 @@ async fn main() -> Result<()> {
         );
     }
 
-    let token = get_erc20_info(&token, &client).await?;
-    if logs_enabled {
-        println!("Token address: {}", token.address);
-        println!("Token name: {}", token.name);
-        println!("Token symbol: {}", token.symbol);
-        println!("Token decimals: {}", token.decimals);
-        println!("");
-    }
-
-    let univ2_pair = get_weth_pair(&token.address, &client).await?;
-
-    if logs_enabled {
-        println!("WETH/{} UniV2 pair: {}", token.symbol, univ2_pair.address);
-    }
-
-    test_token(univ2_pair, token, sender, client, logs_enabled)
-        .await
-        .map_err(|e| anyhow!("Test failed, token is most likely a honeypot: {e}"))?;
+    test_swap(&mut get_cache_db(client)?)?;
 
     Ok(())
+
+    // let token = get_erc20_info(&token, &client).await?;
+    // if logs_enabled {
+    //     println!("Token address: {}", token.address);
+    //     println!("Token name: {}", token.name);
+    //     println!("Token symbol: {}", token.symbol);
+    //     println!("Token decimals: {}", token.decimals);
+    //     println!("");
+    // }
+
+    // let univ2_pair = get_weth_pair(&token.address, &client).await?;
+
+    // if logs_enabled {
+    //     println!("WETH/{} UniV2 pair: {}", token.symbol, univ2_pair.address);
+    // }
+
+    // test_token(univ2_pair, token, sender, client, logs_enabled)
+    //     .await
+    //     .map_err(|e| anyhow!("Test failed, token is most likely a honeypot: {e}"))?;
+
+    // Ok(())
 }
 
 // #[tokio::main]
@@ -137,9 +144,9 @@ async fn test_token(
     );
 
     if logs_enabled {
-        let weth_balance_before = balance_of(WETH, sender, &mut cache_db)?;
+        let weth_balance_before = balance_of(WETH, sender, sender, &mut cache_db)?;
         println!("WETH balance before swap: {}", weth_balance_before);
-        let token_balance_before = balance_of(token.address, sender, &mut cache_db)?;
+        let token_balance_before = balance_of(token.address, sender, sender, &mut cache_db)?;
         println!(
             "{} balance before swap: {}",
             token.symbol, token_balance_before
@@ -147,14 +154,14 @@ async fn test_token(
     }
 
     let amount_in = one_eth.div_ceil(U256::from(10));
-    let reserves = get_reserves(pair.address, &mut cache_db)?;
+    let reserves = get_univ2_reserves(pair.address, sender, &mut cache_db)?;
 
     // 2. Swap WETH for Token
     let amount_out = univ2_swap(sender, &pair, WETH, amount_in, reserves, &mut cache_db)?;
 
     // 3. Swap Token for WETH
     //    this is what shows if the token is a honeypot or not.
-    let reserves = get_reserves(pair.address, &mut cache_db)?;
+    let reserves = get_univ2_reserves(pair.address, sender, &mut cache_db)?;
     univ2_swap(
         sender,
         &pair,
@@ -165,9 +172,9 @@ async fn test_token(
     )?;
 
     if logs_enabled {
-        let weth_balance_after = balance_of(WETH, sender, &mut cache_db)?;
+        let weth_balance_after = balance_of(WETH, sender, sender, &mut cache_db)?;
         println!("WETH balance after swap: {}", weth_balance_after);
-        let token_balance_after = balance_of(token.address, sender, &mut cache_db)?;
+        let token_balance_after = balance_of(token.address, sender, sender, &mut cache_db)?;
         println!(
             "{} balance after swap: {}",
             token.symbol, token_balance_after
@@ -182,203 +189,4 @@ async fn test_token(
 fn get_cache_db(client: RootProvider<Http<Client>>) -> Result<AlloyCacheDB> {
     let db = AlloyDB::new(client, BlockId::latest()).expect("Failed to create Revm Alloy DB");
     Ok(CacheDB::new(db))
-}
-
-fn balance_of(token: Address, address: Address, alloy_db: &mut AlloyCacheDB) -> Result<U256> {
-    sol! {
-        function balanceOf(address account) public returns (uint256);
-    }
-
-    let encoded = balanceOfCall { account: address }.abi_encode();
-
-    let mut evm = Evm::builder()
-        .with_db(alloy_db)
-        .modify_tx_env(|tx| {
-            tx.caller = address!("0000000000000000000000000000000000000001");
-            tx.transact_to = TxKind::Call(token);
-            tx.data = encoded.into();
-            tx.value = U256::from(0);
-        })
-        .build();
-
-    let ref_tx = evm.transact().unwrap();
-    let result = ref_tx.result;
-
-    let value = match result {
-        ExecutionResult::Success {
-            output: Output::Call(value),
-            ..
-        } => value,
-        result => return Err(anyhow!("'balanceOf' execution failed: {result:?}")),
-    };
-
-    let balance = <U256>::abi_decode(&value, false)?;
-
-    Ok(balance)
-}
-
-fn transfer(
-    from: Address,
-    to: Address,
-    amount: U256,
-    token: Address,
-    cache_db: &mut AlloyCacheDB,
-) -> Result<()> {
-    sol! {
-        function transfer(address to, uint amount) external returns (bool);
-    }
-
-    let encoded = transferCall { to, amount }.abi_encode();
-
-    let mut evm = Evm::builder()
-        .with_db(cache_db)
-        .modify_tx_env(|tx| {
-            tx.caller = from;
-            tx.transact_to = TxKind::Call(token);
-            tx.data = encoded.into();
-            tx.value = U256::from(0);
-        })
-        .build();
-
-    let ref_tx = evm.transact_commit().unwrap();
-    let success: bool = match ref_tx {
-        ExecutionResult::Success {
-            output: Output::Call(value),
-            ..
-        } => {
-            // This is because some ERC20 tokens do not return true/false
-            if value.len() > 0 {
-                <bool>::abi_decode(&value, false)?
-            } else {
-                true
-            }
-        }
-        result => return Err(anyhow!("'transfer' execution failed: {result:?}")),
-    };
-
-    if !success {
-        return Err(anyhow!("'transfer' failed"));
-    }
-
-    Ok(())
-}
-
-fn swap(
-    from: Address,
-    pool_address: Address,
-    amount_out: U256,
-    is_token0: bool,
-    cache_db: &mut AlloyCacheDB,
-) -> Result<()> {
-    sol! {
-        function swap(uint amount0Out, uint amount1Out, address target, bytes callback) external;
-    }
-
-    let amount0_out = if is_token0 { amount_out } else { U256::from(0) };
-    let amount1_out = if is_token0 { U256::from(0) } else { amount_out };
-
-    let encoded = swapCall {
-        amount0Out: amount0_out,
-        amount1Out: amount1_out,
-        target: from,
-        callback: Bytes::new(),
-    }
-    .abi_encode();
-
-    let mut evm = Evm::builder()
-        .with_db(cache_db)
-        .modify_tx_env(|tx| {
-            tx.caller = from;
-            tx.transact_to = TxKind::Call(pool_address);
-            tx.data = encoded.into();
-            tx.value = U256::from(0);
-            // tx.nonce = Some(1);
-        })
-        .build();
-
-    let ref_tx = evm.transact_commit().unwrap();
-
-    match ref_tx {
-        ExecutionResult::Success { .. } => {}
-        result => return Err(anyhow!("'swap' execution failed: {result:?}")),
-    };
-
-    Ok(())
-}
-
-fn get_reserves(pair_address: Address, cache_db: &mut AlloyCacheDB) -> Result<(U256, U256)> {
-    sol! {
-        function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    }
-
-    let encoded = getReservesCall {}.abi_encode();
-
-    let mut evm = Evm::builder()
-        .with_db(cache_db)
-        .modify_tx_env(|tx| {
-            tx.caller = address!("0000000000000000000000000000000000000000");
-            tx.transact_to = TxKind::Call(pair_address);
-            tx.data = encoded.into();
-            tx.value = U256::from(0);
-        })
-        .build();
-
-    let ref_tx = evm.transact().unwrap();
-    let result = ref_tx.result;
-
-    let value = match result {
-        ExecutionResult::Success {
-            output: Output::Call(value),
-            ..
-        } => value,
-        result => return Err(anyhow!("'getReserves' execution failed: {result:?}")),
-    };
-
-    let (reserve0, reserve1, _) = <(U256, U256, u32)>::abi_decode(&value, false)?;
-
-    Ok((reserve0, reserve1))
-}
-
-async fn get_amount_out(
-    amount_in: U256,
-    reserve_in: U256,
-    reserve_out: U256,
-    cache_db: &mut AlloyCacheDB,
-) -> Result<U256> {
-    let uniswap_v2_router = address!("7a250d5630b4cf539739df2c5dacb4c659f2488d");
-    sol! {
-        function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
-    }
-
-    let encoded = getAmountOutCall {
-        amountIn: amount_in,
-        reserveIn: reserve_in,
-        reserveOut: reserve_out,
-    }
-    .abi_encode();
-
-    let mut evm = Evm::builder()
-        .with_db(cache_db)
-        .modify_tx_env(|tx| {
-            tx.caller = address!("0000000000000000000000000000000000000000");
-            tx.transact_to = TxKind::Call(uniswap_v2_router);
-            tx.data = encoded.into();
-            tx.value = U256::from(0);
-        })
-        .build();
-
-    let ref_tx = evm.transact().unwrap();
-    let result = ref_tx.result;
-
-    let value = match result {
-        ExecutionResult::Success {
-            output: Output::Call(value),
-            ..
-        } => value,
-        result => return Err(anyhow!("'getAmountOut' execution failed: {result:?}")),
-    };
-
-    let amount_out = <U256>::abi_decode(&value, false)?;
-
-    Ok(amount_out)
 }
---
+++ src/uniswapv3.rs
@@ -0,0 +1,188 @@
+// sqrtPriceLimitX96 == 0
+//     ? (zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1)
+//     : sqrtPriceLimitX96,
+
+// /// @dev The minimum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**-128
+// int24 internal constant MIN_TICK = -887272;
+// /// @dev The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
+// int24 internal constant MAX_TICK = -MIN_TICK;
+
+use alloy::primitives::aliases::U24;
+use alloy::primitives::U160;
+use alloy::sol;
+use alloy::sol_types::{SolCall, SolValue};
+use anyhow::{anyhow, Result};
+use revm::primitives::{
+    address, keccak256, AccountInfo, Address, Bytes, ExecutionResult, TxKind, I256, U256,
+};
+use revm::Evm;
+
+use crate::revm_actions::{balance_of, transfer};
+use crate::{AlloyCacheDB, DEFAULT_ACC, WETH};
+
+const ROUTER: Address = address!("E592427A0AEce92De3Edee1F18E0157C05861564");
+
+sol! {
+    function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes calldata data) external returns (int256 amount0, int256 amount1);
+
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint160 sqrtPriceLimitX96;
+    }
+    function exactInputSingle(ExactInputSingleParams calldata params) external returns (uint256 amountOut);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+pub fn test_swap(db: &mut AlloyCacheDB) -> Result<()> {
+    let sender = DEFAULT_ACC;
+    // USDC/WETH
+    let weth_usdc_500_pool = address!("88e6a0c2ddd26feeb64f039a2c41296fcb3f5640");
+    let usdc = address!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+    // Swap from WETH to USDC
+    let zero_for_one = false;
+    // 0.1 WETH
+    // let amount_specified = I256::try_from(1 * 10u128.pow(18) / 10).unwrap();
+    let amount_specified = U256::try_from(1 * 10u128.pow(18) / 10).unwrap();
+
+    // Adding WETH to sender
+    let weth_balance_slot = U256::from(3);
+    let one_eth = U256::from(10_u128.pow(18));
+    let weth_user_balance_slot = keccak256((sender, weth_balance_slot).abi_encode());
+    db.insert_account_storage(WETH, weth_user_balance_slot.into(), one_eth)
+        .expect("Failed to insert account storage");
+    db.insert_account_info(
+        sender,
+        AccountInfo {
+            balance: one_eth,
+            ..Default::default()
+        },
+    );
+
+    // transfer(sender, weth_usdc_500_pool, one_eth, WETH, db)?;
+    approve(WETH, sender, one_eth, db)?;
+
+    univ3_swap(
+        weth_usdc_500_pool,
+        usdc,
+        sender,
+        zero_for_one,
+        amount_specified,
+        db,
+    )?;
+
+    let usdc_balance_after = balance_of(usdc, sender, sender, db)?;
+    println!("USDC balance after swap: {}", usdc_balance_after);
+
+    Ok(())
+}
+
+fn univ3_swap(
+    pool: Address,
+    token: Address,
+    sender: Address,
+    zero_for_one: bool,
+    amount_specified: U256,
+    db: &mut AlloyCacheDB,
+) -> Result<()> {
+    let min_sqrt_ratio: U160 = U160::from(4295128739_u64);
+    let max_sqrt_ratio: U160 = "1461446703485210103287273052203988822378723970342"
+        .parse()
+        .unwrap();
+
+    let sqrt_price_limit_x96 = if zero_for_one {
+        min_sqrt_ratio.checked_add(U160::from(1)).unwrap()
+    } else {
+        max_sqrt_ratio.checked_sub(U160::from(1)).unwrap()
+    };
+
+    // let amount_specified = if zero_for_one {
+    //     amount_specified
+    // } else {
+    //     -amount_specified
+    // };
+
+    println!("sqrt_price_limit_x96: {}", sqrt_price_limit_x96);
+    println!("amount_specified: {}", amount_specified);
+
+    // let calldata = swapCall {
+    //     recipient: DEFAULT_ACC,
+    //     zeroForOne: zero_for_one,
+    //     amountSpecified: amount_specified,
+    //     sqrtPriceLimitX96: sqrt_price_limit_x96,
+    //     data: Bytes::default(),
+    // }
+    // .abi_encode();
+    let params = ExactInputSingleParams {
+        tokenIn: WETH,
+        tokenOut: token,
+        fee: U24::from(500),
+        recipient: sender,
+        deadline: U256::from(1111111111),
+        amountIn: amount_specified,
+        amountOutMinimum: U256::from(0),
+        // sqrtPriceLimitX96: sqrt_price_limit_x96,
+        sqrtPriceLimitX96: U160::from(0),
+    };
+
+    let calldata = exactInputSingleCall { params }.abi_encode();
+
+    let token_balance_before = balance_of(token, sender, sender, db)?;
+    println!("USDC balance before swap: {}", token_balance_before);
+
+    let mut evm = Evm::builder()
+        .with_db(db)
+        .modify_tx_env(|tx| {
+            tx.caller = sender;
+            tx.transact_to = TxKind::Call(ROUTER);
+            tx.data = calldata.into();
+            // tx.gas_limit = 100_000_000;
+        })
+        .build();
+
+    let tx = evm
+        .transact_commit()
+        .map_err(|e| anyhow!("Failed to execute swap call on Uniswap V3 pair: {e}"))?;
+
+    match tx {
+        ExecutionResult::Success { .. } => Ok(()),
+        result => {
+            println!("Swap failed: {:?}", result);
+            return Err(anyhow!(
+                "'swap' execution failed on Uniswap V2 pair: {result:?}"
+            ));
+        }
+    }
+}
+
+fn approve(token: Address, sender: Address, amount: U256, db: &mut AlloyCacheDB) -> Result<()> {
+    let calldata = approveCall {
+        spender: ROUTER,
+        amount,
+    }
+    .abi_encode();
+
+    let mut evm = Evm::builder()
+        .with_db(db)
+        .modify_tx_env(|tx| {
+            tx.caller = sender;
+            tx.transact_to = TxKind::Call(token);
+            tx.data = calldata.into();
+        })
+        .build();
+
+    let tx = evm
+        .transact_commit()
+        .map_err(|e| anyhow!("Failed to execute transfer call: {e}"))?;
+
+    match tx {
+        ExecutionResult::Success { .. } => return Ok(()),
+        result => return Err(anyhow!("'approve' execution failed: {result:?}")),
+    };
+}
